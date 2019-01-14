9828a708a8ef27a72768ef0358e9c3cd0d760bf7
JoshuaWong <1034316376@qq.com>
2019-01-14 14:24:12 +0800

    Update IdPool.java

--- src/com/joshua/glframeworklite/IdPool.java
+++ src/com/joshua/glframeworklite/IdPool.java
@@ -11,27 +11,34 @@ import java.util.Deque;
  */
 public class IdPool {
     private static final Deque<int[]> mIdsList = new ArrayDeque<>();
-    private static final int MAX_SIZE = 128;
+    private static final int POOL_LIMIT = 1024;
 
-    public static int[] getTextureIds() {
+    public static synchronized int[] getTextureIds() {
         int[] ids = mIdsList.size() > 0 ? mIdsList.remove() : new int[1];
         ids[0] = Constants.INVALID_TEXTURE;
         return ids;
     }
 
-    public static int[] getBufferIds() {
+    public static synchronized int[] getBufferIds() {
         int[] ids = mIdsList.size() > 0 ? mIdsList.remove() : new int[1];
         ids[0] = Constants.INVALID_BUFFER;
         return ids;
     }
 
-    public static void recycle(int[] ids) {
-        if (mIdsList.size() < MAX_SIZE) {
-            mIdsList.addFirst(ids);
+    public static synchronized void recycle(int[] ids) {
+        if(id == null){
+            return;
+        }
+        if(id.length!=1){
+            return;
+        }
+        
+        if (mIdsList.size() < POOL_LIMIT) {
+            mIdsList.offer(ids);
         }
     }
 
-    public static void clear() {
+    public static synchronized void clear() {
         mIdsList.clear();
     }
 }
