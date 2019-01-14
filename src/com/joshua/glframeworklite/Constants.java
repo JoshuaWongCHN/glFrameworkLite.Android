e691d2214718649a85a8ce62b5e02ad5e70f41f3
JoshuaWong <1034316376@qq.com>
2019-01-14 14:20:10 +0800

    Update Constants.java

--- src/com/joshua/glframeworklite/Constants.java
+++ src/com/joshua/glframeworklite/Constants.java
@@ -7,7 +7,10 @@ package com.joshua.glframeworklite;
  * @Version 1.0
  */
 public class Constants {
+    public static final int INVALID_PROGRAM = 0;
+    public static final int INVALID_SHADER = 0
     public static final int INVALID_TEXTURE = 0;
     public static final int INVALID_BUFFER = -1;
+    public static final int INVALID_HANDLE = -1;
 }
 
